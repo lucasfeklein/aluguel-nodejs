2f6ca7c24650db6ac5bc27d6f99e70a0f9f06296
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-02 14:39:36 +0800

    prisma client working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { prisma } from "./prisma.js";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 app.listen(port, () => {
