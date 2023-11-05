6094c9b2d0f0d9ae3b205a7a1043e05f02ae0c6f
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-05 09:57:34 +0800

    AUTH ROUTER

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from "express";
-import { bookRouter, copyRouter, personRouter } from "./routes/index.js";
+import {
+  authRouter,
+  bookRouter,
+  copyRouter,
+  personRouter,
+} from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.use("/book", bookRouter);
 app.use("/person", personRouter);
 app.use("/copy", copyRouter);
+app.use("/auth", authRouter);
 
 app.get("/", async (req, res) => {
   res.send("oi");
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
+import authRouter from "./auth.js";
 import bookRouter from "./book.js";
 import copyRouter from "./copy.js";
 import personRouter from "./person.js";
 
-export { bookRouter, copyRouter, personRouter };
+export { authRouter, bookRouter, copyRouter, personRouter };
