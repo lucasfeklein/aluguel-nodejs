b3753bb2f62ee7e468b67fafb4c416d72ce65e81
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-07 15:31:36 +0800

    rent copy route

--- routes/copy.js
+++ routes/copy.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { verifyToken } from "../middleware.js";
 import { prisma } from "../prisma.js";
 
 const router = express.Router();
@@ -8,6 +9,35 @@ router.get("/", async (req, res) => {
   res.json(books);
 });
 
-router.post("/rentcopy", async (req, res) => {});
+router.post("/rentcopy", verifyToken, async (req, res) => {
+  const { copyId } = req.body;
+
+  const pin = req.pin;
+
+  const copy = await prisma.copy.findUnique({ where: { id: copyId } });
+
+  // Check if the copy is already rented
+  if (copy.isRented) {
+    res.status(400).json({ error: "Copy is already rented" });
+  }
+
+  const currentDate = new Date(); // Get the current date and time
+  const rentedDate = new Date(currentDate.toDateString()); // Extract the date part
+
+  const updatedCopy = await prisma.copy.update({
+    where: { id: copyId },
+    data: { isRented: true, rentedByPin: pin },
+  });
+
+  const rentalHistory = await prisma.rentalHistory.create({
+    data: {
+      copyId: copyId,
+      rentedByPin: pin,
+      rentedAt: rentedDate,
+    },
+  });
+
+  res.json({ updatedCopy });
+});
 
 export default router;
